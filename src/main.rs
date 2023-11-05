c8b5a1ed7215dbea11954c8e5d1b8d1a002e6890
luckychacha <358140284@qq.com>
2023-11-06 01:40:55 +0800

    Try to screenshot but still has some problem.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
 // SPDX-License-Identifier: MIT
 
+use core::time;
+use std::thread;
+
 use i_slint_backend_winit::WinitWindowAccessor;
-use slint::{LogicalPosition, LogicalSize, WindowPosition, WindowSize};
+use screenshots::Screen;
+use slint::{
+    Image, LogicalPosition, LogicalSize, Rgba8Pixel, SharedPixelBuffer, WindowPosition, WindowSize,
+};
 
 #[cfg(target_arch = "wasm32")]
 use wasm_bindgen::prelude::*;
@@ -68,14 +74,14 @@ pub fn main() {
         main_window.on_stop_pick_screen_color(move || {
             let window = weak_window.unwrap();
             if let Some((screen_width, screen_height)) = get_current_monitor_size(window.window()) {
-            set_window_position_and_size(
-                window.window(),
-                400.0,
-                600.0,
-                (screen_width / 2.0 - 200.0) as i32,
-                (screen_height / 2.0 - 300.0) as i32,
-            );
-                }
+                set_window_position_and_size(
+                    window.window(),
+                    400.0,
+                    600.0,
+                    (screen_width / 2.0 - 200.0) as i32,
+                    (screen_height / 2.0 - 300.0) as i32,
+                );
+            }
         });
     }
 
@@ -83,17 +89,67 @@ pub fn main() {
         let weak_window = main_window.as_weak();
         main_window.on_mouse_move(move |circle_position| {
             println!("cursor position: {:?}", circle_position.as_str());
-            if circle_position.as_str() != "0,0" {
-                let window = weak_window.unwrap();
+            let window = weak_window.unwrap();
+            window.set_is_capturing(true);
+            window.set_circle_position_x(-100.0);
+            window.set_circle_position_y(-100.0);
+            window.set_has_image(false);
+            if !window.get_has_image() && circle_position.as_str() != "0,0" {
+                
                 let pos: Vec<f32> = circle_position
                     .as_str()
                     .split(",")
                     .map(|e| e.parse::<f32>().unwrap())
                     .collect();
-                window.set_circle_position_x(pos[0] - window.get_picker_circle_radius() as f32);
-                window.set_circle_position_y(pos[1] - window.get_picker_circle_radius() as f32);
+                let circle_position_x = pos[0] - window.get_picker_circle_radius() as f32;
+                let circle_position_y = pos[1] - window.get_picker_circle_radius() as f32;
+                
                 if window.get_cursor_position_changed() {
                     window.set_moving(true);
+
+                    
+                    println!(
+                        "before image: {:?} get_picking_color_on_screen: {:?} get_moving: {:?} get_is_capturing: {:?} ",
+                        window.get_picking_color_on_screen() && window.get_moving() && !window.get_is_capturing(),
+                        window.get_picking_color_on_screen(),window.get_moving(),window.get_is_capturing(),
+                    );
+                    let screens = Screen::all().unwrap();
+                    if let Some(image_buffer) = screens
+                        .get(0)
+                        .and_then(|f| {
+                            if !window.get_has_image() {
+                                // if let Ok(image_buffer) = f.capture() {
+                                //     if let Ok(_) = image_buffer.save("test.png") {
+                                //         println!("保存成功");
+                                //     }
+                                // }
+                                f.capture_area(pos[0] as i32, pos[1] as i32 - 20, 30, 30).ok()
+                            } else {
+                                None
+                            }
+                        })
+                    {
+                        let buffer = SharedPixelBuffer::<Rgba8Pixel>::clone_from_slice(
+                            image_buffer.as_raw(),
+                            image_buffer.width(),
+                            image_buffer.height(),
+                        );
+                        window.set_cursor_around_screenshot(Image::from_rgba8(buffer));
+                        if window.get_is_capturing() {
+                            window.set_is_capturing(false);
+                        }
+                        println!("显示");
+                        println!(
+                            "after image: {:?} get_picking_color_on_screen: {:?} get_moving: {:?} get_is_capturing: {:?} ",
+                            window.get_picking_color_on_screen() && window.get_moving() && !window.get_is_capturing(),
+                            window.get_picking_color_on_screen(),window.get_moving(),window.get_is_capturing(),
+                        );
+                        window.set_circle_position_x(circle_position_x);
+                        window.set_circle_position_y(circle_position_y);
+                        if !window.get_has_image() {
+                            window.set_has_image(true);
+                        }
+                    }
                 } else {
                     window.set_cursor_position_changed(true);
                 }
@@ -110,6 +166,8 @@ pub fn main() {
             600.0,
             (screen_width / 2.0 - 200.0) as i32,
             (screen_height / 2.0 - 300.0) as i32,
+            // 0,
+            // 0,
         );
     }
     main_window.run().unwrap();
