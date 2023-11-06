bb221c7ebf0378de3dd865b74948b3d184a017ef
luckychacha <358140284@qq.com>
2023-11-06 11:34:41 +0800

    Another way to show the circle.

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,12 @@ use core::time;
 use std::thread;
 
 use i_slint_backend_winit::WinitWindowAccessor;
+use image::imageops;
 use screenshots::Screen;
 use slint::{
     Image, LogicalPosition, LogicalSize, Rgba8Pixel, SharedPixelBuffer, WindowPosition, WindowSize,
 };
+use slint::private_unstable_api::re_exports::ImageInner;
 
 #[cfg(target_arch = "wasm32")]
 use wasm_bindgen::prelude::*;
@@ -57,6 +59,8 @@ pub fn main() {
 
             window.set_cursor_position_changed(false);
             window.set_moving(false);
+            window.set_has_image(false);
+
             if let Some((screen_width, screen_height)) = get_current_monitor_size(window.window()) {
                 set_window_position_and_size(
                     window.window(),
@@ -73,7 +77,11 @@ pub fn main() {
         let weak_window = main_window.as_weak();
         main_window.on_stop_pick_screen_color(move || {
             let window = weak_window.unwrap();
+            window.set_cursor_position_changed(false);
+            window.set_moving(false);
+            window.set_has_image(false);
             if let Some((screen_width, screen_height)) = get_current_monitor_size(window.window()) {
+                println!("windows has image: {:?}", window.get_has_image());
                 set_window_position_and_size(
                     window.window(),
                     400.0,
@@ -90,70 +98,58 @@ pub fn main() {
         main_window.on_mouse_move(move |circle_position| {
             println!("cursor position: {:?}", circle_position.as_str());
             let window = weak_window.unwrap();
-            window.set_is_capturing(true);
-            window.set_circle_position_x(-100.0);
-            window.set_circle_position_y(-100.0);
+            // window.set_circle_position_x(100.0);
+            // window.set_circle_position_y(100.0);
+            window.set_moving(false);
             window.set_has_image(false);
             if !window.get_has_image() && circle_position.as_str() != "0,0" {
-                
                 let pos: Vec<f32> = circle_position
                     .as_str()
                     .split(",")
                     .map(|e| e.parse::<f32>().unwrap())
                     .collect();
-                let circle_position_x = pos[0] - window.get_picker_circle_radius() as f32;
-                let circle_position_y = pos[1] - window.get_picker_circle_radius() as f32;
-                
-                if window.get_cursor_position_changed() {
-                    window.set_moving(true);
+                // let circle_position_x = pos[0] - window.get_picker_circle_radius() as f32;
+                // let circle_position_y = pos[1] - window.get_picker_circle_radius() as f32;
 
-                    
-                    println!(
-                        "before image: {:?} get_picking_color_on_screen: {:?} get_moving: {:?} get_is_capturing: {:?} ",
-                        window.get_picking_color_on_screen() && window.get_moving() && !window.get_is_capturing(),
-                        window.get_picking_color_on_screen(),window.get_moving(),window.get_is_capturing(),
-                    );
+                let circle_position_x = pos[0] + (window.get_picker_circle_radius() as f32) / 3.0;
+                let circle_position_y = pos[1] + (window.get_picker_circle_radius() as f32) / 3.0;
+
+                // let circle_position_x = 25.0;
+                // let circle_position_y = 25.0;
+
+                if window.get_cursor_position_changed() {
                     let screens = Screen::all().unwrap();
                     if let Some(image_buffer) = screens
                         .get(0)
                         .and_then(|f| {
                             if !window.get_has_image() {
-                                // if let Ok(image_buffer) = f.capture() {
-                                //     if let Ok(_) = image_buffer.save("test.png") {
-                                //         println!("保存成功");
-                                //     }
-                                // }
-                                f.capture_area(pos[0] as i32, pos[1] as i32 - 20, 30, 30).ok()
+                                f.capture_area(pos[0] as i32 - 15, pos[1] as i32 - 20, 30, 30).ok()
                             } else {
                                 None
                             }
                         })
                     {
+                        let image_buffer = imageops::resize(&image_buffer, 90, 90, imageops::FilterType::Nearest);
                         let buffer = SharedPixelBuffer::<Rgba8Pixel>::clone_from_slice(
                             image_buffer.as_raw(),
                             image_buffer.width(),
                             image_buffer.height(),
                         );
+                        // println!("aaa: {:?}", window.get_cursor_around_screenshot());
                         window.set_cursor_around_screenshot(Image::from_rgba8(buffer));
-                        if window.get_is_capturing() {
-                            window.set_is_capturing(false);
-                        }
-                        println!("显示");
-                        println!(
-                            "after image: {:?} get_picking_color_on_screen: {:?} get_moving: {:?} get_is_capturing: {:?} ",
-                            window.get_picking_color_on_screen() && window.get_moving() && !window.get_is_capturing(),
-                            window.get_picking_color_on_screen(),window.get_moving(),window.get_is_capturing(),
-                        );
+
                         window.set_circle_position_x(circle_position_x);
                         window.set_circle_position_y(circle_position_y);
                         if !window.get_has_image() {
                             window.set_has_image(true);
+                            window.set_moving(true);
                         }
                     }
                 } else {
                     window.set_cursor_position_changed(true);
+                    window.set_has_image(false);
                 }
-                println!("moving: {:?}", window.get_moving());
+                println!("moving: {:?} has_image: {:?}", window.get_moving(), window.get_has_image());
             }
             "".into()
         });
@@ -181,6 +177,6 @@ fn android_main(app: i_slint_backend_android_activity::AndroidApp) {
             eprintln!("Got event: {event:?}")
         }),
     ))
-    .unwrap();
+        .unwrap();
     main();
 }
